aae93a8d78541c99f2b6c664d1513095ab17b554
alisrom <acortes@supermas.mx>
2015-06-05 10:37:34 -0500

    añadiendo depuracion

--- alicia/codigos/__openerp__.py
+++ alicia/codigos/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 {
-  'name': 'SM Codigo',
+  'name': 'SM Códigos',
   'version': '1.0',
   'category': 'SUPERMAS',
   'author': 'SUPERMAS',
@@ -9,7 +9,23 @@
   'website': 'http://www.supermas.mx',
   'installable': True, 
   'active': False,
-  'description': 'Módulo Codigos- Modulo desarrollado para generar las etiquetas automaticamente de cada producto atraves de una lista codigos EAN-13',   
+  'description': """
+      Modulo: SM Codigos - Generador de Etiquetas.
+      ====================================
+      
+      Modulo desarrollado para generar las etiquetas automaticamente de cada producto a traves de una lista codigos EAN-13 contiene las siguientes
+      secciones:
+      --------------------------------------------
+          * Generador de codigos - Wizard que genera las etiquetas a partir de la insercion de una lista de codigos en el área de texto.
+          * Listado de codigos - Muestra los datos relacionados con los códigos previamente insertados en el generador de codigos.
+         
+      
+      El modulo requiere de la instalación especial de la libreria:
+      --------------------------------------------------
+          * pyBarcode
+          
+      Crea el codigo de barras como imagen, Se puede consultar para su instalacion el siguiente Link: https://pypi.python.org/pypi/pyBarcode/0.7
+    """,
   #This model depends of BASE OpenERP model...
   'depends': [
     'base',
